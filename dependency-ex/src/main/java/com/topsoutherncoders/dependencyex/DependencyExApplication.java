017ac15cd0ffe83a40acb275d7b4468068e5086b
onesirian <ldlockhart@gmail.com>
2023-07-05 18:00:36 -0500

    add spring boot starter

---
+++ dependency-ex/src/main/java/com/topsoutherncoders/dependencyex/DependencyExApplication.java
@@ -0,0 +1,13 @@
+package com.topsoutherncoders.dependencyex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DependencyExApplication.class, args);
+    }
+
+}
---
+++ dependency-ex/src/test/java/com/topsoutherncoders/dependencyex/DependencyExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topsoutherncoders.dependencyex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
